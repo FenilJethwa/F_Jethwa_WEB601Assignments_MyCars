e2370ae4a0716fa9058c4000d3ecbf3d1ce5ad18
FenilJethwa <w0805286@myscc.ca>
2023-04-21 13:57:05 -0400

    Add function to add & clear msg and show in html

--- F_Jethwa_MyCars/src/app/message/message.component.ts
+++ F_Jethwa_MyCars/src/app/message/message.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MessageService } from '../services/message.service';
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
