f411dc7b2cac78a675c8695b55aadd8601e6bf13
FenilJethwa <w0805286@myscc.ca>
2023-04-21 18:53:24 -0400

    Edit code

--- F_Jethwa_MyCars/src/app/app.module.ts
+++ F_Jethwa_MyCars/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { AddContentDialogComponent } from './add-content-dialog/add-content-dialog.component';
 
 @NgModule({
