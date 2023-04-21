d7b93d2977617d9f57df08b19ab32272b5f27044
FenilJethwa <w0805286@myscc.ca>
2023-04-21 10:33:09 -0400

    Create new foldewr as create-content in app

---
+++ F_Jethwa_MyCars/src/app/create-content/create-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-create-content',
+  templateUrl: './create-content.component.html',
+  styleUrls: ['./create-content.component.scss']
+})
+  
+export class CreateContentComponent {
+
+}
