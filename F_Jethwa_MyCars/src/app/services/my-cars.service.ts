e3e277a412779cc41e71d2820b289b4a2c8618d3
FenilJethwa <w0805286@myscc.ca>
2023-04-21 14:02:43 -0400

    Add msg and css

--- F_Jethwa_MyCars/src/app/services/my-cars.service.ts
+++ F_Jethwa_MyCars/src/app/services/my-cars.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Content } from '../helper-files/content-interface';
 import { contents } from '../helper-files/ContentDb';
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MyCarsService {
 
-  constructor() { }
+ constructor(private MessageService: MessageService) { }
   getCars(): Observable<Content[]> {
     const cars = contents;
+    this.MessageService.add("Content array loaded!");
     return of(cars);
   }
   getCarById(id: number): Observable<any> {
     const car = contents.find(content => content.id === id);
+    this.MessageService.add(`Content Item at id: ${id}`);
     return of(car);
   }
 }
