149d8e4ca197561c94860589dbb29d26d24fe167
FenilJethwa <w0805286@myscc.ca>
2023-04-22 18:59:15 -0400

    Update Version abd add file of routing

--- F_Jethwa_MyCars/src/app/app.module.ts
+++ F_Jethwa_MyCars/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
@@ -36,6 +36,7 @@ import { AddContentDialogComponent } from './add-content-dialog/add-content-dial
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     HttpClientModule,
     MatInputModule,
