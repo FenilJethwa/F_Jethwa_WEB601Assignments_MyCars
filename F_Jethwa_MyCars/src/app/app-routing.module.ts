a404964d8430e8d11bfc68e0d4da2de72b5478b1
FenilJethwa <w0805286@myscc.ca>
2023-04-22 21:18:18 -0400

    commit

--- F_Jethwa_MyCars/src/app/app-routing.module.ts
+++ F_Jethwa_MyCars/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentDetailComponent } from './content-detail/content-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,10 @@ const routes: Routes = [
   {
     path: 'contents/:id',
     component: ContentDetailComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
 
